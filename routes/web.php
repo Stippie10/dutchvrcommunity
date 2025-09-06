736b7f3f4681a9b9bf32f726716734607bd8176f
Stippie10 <stippienine10@gmail.com>
2025-09-06 19:23:09 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,3 +35,5 @@ Route::get('/team', function () {
 Route::get('/events', function () {
     return view('events');
 });
+
+Auth::routes();
