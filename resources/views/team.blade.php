37db32a07de039801789d521754be2e80959132f
Stippie10 <stippienine10@gmail.com>
2025-09-06 15:29:33 +0200

    Update team.blade.php

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -129,7 +129,7 @@
                 <!-- Voorbeeld Teamlid 1 -->
                 <div class="team-member">
                     <img src="https://i.pravatar.cc/150?img=1" alt="Teamlid 1">
-                    <h3>Stippie</h3>
+                    <h3>Cheesy dragon</h3>
                     <p>Oprichter en VR-enthousiasteling. Altijd op zoek naar de nieuwste hardware en ervaringen.</p>
                 </div>
                 <!-- Voorbeeld Teamlid 2 -->
